71bf0bdbab7e06696231ff255f66d8181105f5ee
AlmazerX <79789378+AlmazerX@users.noreply.github.com>
2021-03-16 00:05:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
-exports.min = function min (array) {
-  return 0;
+function max(arr){
+ var max = arr[0];
+ for(var i=1; i<arr.length; i++){
+   if(arr[i] > max){
+     max = arr[i];   
+   }
+  }
+return max;
+}
+function min(arr){
+ var min = arr[0];
+ for(var i=1; i<arr.length; i++){
+   if(arr[i] < min){
+     min = arr[i];   
+   }
+  }
+return min;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+function avg(arr){
+ var avg;
+ var sum = 0;
+ for(var i=0; i<arr.length; i++){
+   sum+=arr[i];
+  }
+return avg=sum/arr.length;
 }
